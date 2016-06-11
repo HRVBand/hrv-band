adbcf10a0f2fb7d5be3d87be6d1c064092886d42
Fenox <fenox@hotmail.de>
2016-06-11 18:03:18 +0200

    Baevsky wert

--- src/inno/hacks/ms/band/Control/Calculation.java
+++ src/inno/hacks/ms/band/Control/Calculation.java
@@ -2,6 +2,10 @@ package inno.hacks.ms.band.Control;
 
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import inno.hacks.ms.band.Fourier.IFourierTransformation;
 import inno.hacks.ms.band.Interpolation.IInterpolation;
 import inno.hacks.ms.band.RRInterval.Interval;
@@ -20,7 +24,7 @@ public class Calculation {
         interpolation = interpol;
     }
 
-    public void Calculate(Interval rrinterval)
+    public HRVParameters Calculate(Interval rrinterval)
     {
         double[] y = rrinterval.GetRRInterval();
         double[] x = new double[y.length];
@@ -57,7 +61,6 @@ public class Calculation {
             betrag[i] = realPart[i] * realPart[i] + complexPart[i] * complexPart[i];
         }
 
-        //
         double maxAbtastFrequenz = 0.5 * (1 / stepSize);
         double frequencySteps = maxAbtastFrequenz / (numInterpolVals - 1);
         double[] frequencies = new double[numInterpolVals];
@@ -67,12 +70,114 @@ public class Calculation {
             frequencies[i] = frequencySteps * i;
         }
 
-
         double hfPow = HfPow(frequencies, betrag, stepSize);
         double lfPow = LfPow(frequencies, betrag, stepSize);
 
         double lfhfRatio = lfPow / hfPow;
         double a = lfhfRatio;
+
+        HRVParameters params = new HRVParameters();
+        params.setHf(hfPow);
+        params.setLf(lfPow);
+        params.setLfhfRatio(lfhfRatio);
+        params.setRmssd(RMSSD(y));
+        params.setSdnn(SDNN(y));
+        params.setSd1(SD1(params.getSdnn()));
+        params.setSd2(SD2(params.getSdnn()));
+        params.setSd1sd2Ratio(SD1SD2(params.getSd1(), params.getSd2()));
+        params.setBaevsky(Baevsky(y));
+
+        return params;
+    }
+
+    public double Baevsky(double[] rrinterval)
+    {
+        double erwartungswert  = Erwartungswert(rrinterval);
+        double min = min(rrinterval);
+        double max = max(rrinterval);
+
+        double baevsky = StatistischeHäufigkeit(rrinterval, erwartungswert, 0.02) / (2 * erwartungswert * (max - min));
+        return baevsky;
+    }
+
+    public double StatistischeHäufigkeit(double[] rrinterval, double erwartungswert, double range)
+    {
+        int counter = 0;
+        for(int i = 0; i < rrinterval.length; i++)
+        {
+            if(rrinterval[i] < erwartungswert + range && rrinterval[i] > erwartungswert - range)
+                counter++;
+        }
+        return counter / (double) rrinterval.length;
+    }
+
+    public double min(double[] array) {
+        double min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            min = Math.min(min, array[i]);
+        }
+        return min;
+    }
+
+    public double max(double[] array) {
+        double max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            max = Math.max(max, array[i]);
+        }
+        return max;
+    }
+
+    public double Erwartungswert(double[] values)
+    {
+        double sum = 0;
+        for(int i = 0; i < values.length; i++)
+        {
+            sum += values[i];
+        }
+
+        double erwartungswert = sum / values.length;
+        return erwartungswert;
+    }
+
+    public double SDNN(double[] rrinterval)
+    {
+        double erwartungswert = Erwartungswert(rrinterval);
+
+        double sum2 = 0;
+        for(int i = 0; i < rrinterval.length; i++)
+        {
+            sum2 += (rrinterval[i] - erwartungswert) * (rrinterval[i] - erwartungswert);
+        }
+
+        double sdnn = Math.sqrt(sum2 / rrinterval.length);
+        return sdnn;
+    }
+
+    public double SD1(double sdnnValue)
+    {
+        return Math.sqrt(0.5 * sdnnValue * sdnnValue);
+    }
+
+    public double SD2(double sdnnValue)
+    {
+        return Math.sqrt(2 * sdnnValue * sdnnValue - 0.5 * sdnnValue * sdnnValue);
+    }
+
+    public double SD1SD2(double sd1, double sd2)
+    {
+        return sd1 / sd2;
+    }
+
+    public double RMSSD(double[] rrinterval)
+    {
+        double sum = 0;
+        for(int i = 1; i < rrinterval.length; i++)
+        {
+            sum += (rrinterval[i-1] - rrinterval[i]) * (rrinterval[i-1] - rrinterval[i]);
+        }
+
+        double rmssd = sum / (rrinterval.length - 1);
+        return rmssd;
     }
 
     public double HfPow(double[] frequencies, double[] betrag, double stepSize)
---
+++ src/inno/hacks/ms/band/Control/HRVParameters.java
@@ -0,0 +1,89 @@
+package inno.hacks.ms.band.Control;
+
+/**
+ * Created by Julian on 11.06.2016.
+ */
+public class HRVParameters {
+
+    private double sd1;
+    private double sd2;
+    private double sd1sd2Ratio;
+    private double lf;
+    private double hf;
+    private double lfhfRatio;
+    private double rmssd;
+    private double sdnn;
+    private double baevsky;
+
+    public double getBaevsky() {
+        return baevsky;
+    }
+
+    public void setBaevsky(double baevsky) {
+        this.baevsky = baevsky;
+    }
+
+    public double getSd1() {
+        return sd1;
+    }
+
+    public void setSd1(double sd1) {
+        this.sd1 = sd1;
+    }
+
+    public double getSd2() {
+        return sd2;
+    }
+
+    public void setSd2(double sd2) {
+        this.sd2 = sd2;
+    }
+
+    public double getSd1sd2Ratio() {
+        return sd1sd2Ratio;
+    }
+
+    public void setSd1sd2Ratio(double sd1sd2Ratio) {
+        this.sd1sd2Ratio = sd1sd2Ratio;
+    }
+
+    public double getLf() {
+        return lf;
+    }
+
+    public void setLf(double lf) {
+        this.lf = lf;
+    }
+
+    public double getHf() {
+        return hf;
+    }
+
+    public void setHf(double hf) {
+        this.hf = hf;
+    }
+
+    public double getLfhfRatio() {
+        return lfhfRatio;
+    }
+
+    public void setLfhfRatio(double lfhfRatio) {
+        this.lfhfRatio = lfhfRatio;
+    }
+
+    public double getRmssd() {
+        return rmssd;
+    }
+
+    public void setRmssd(double rmssd) {
+        this.rmssd = rmssd;
+    }
+
+    public double getSdnn() {
+        return sdnn;
+    }
+
+    public void setSdnn(double sdnn) {
+        this.sdnn = sdnn;
+    }
+}
