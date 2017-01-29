3f37286fa7d16da8e7a72447d391912eb3fc7b4d
Fenox <fenox@hotmail.de>
2017-01-29 17:58:36 +0100

    First commit of branch calc

--- hrvlib/src/main/java/com/example/MyClass.java
+++ hrvlib/src/main/java/com/example/MyClass.java
@@ -3,6 +3,6 @@ package com.example;
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 2");
     }
 }
